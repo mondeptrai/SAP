44024075b9d6f4c2218aa5dacc56785b3ceca9b3
mondeptrai <49212396+mondeptrai@users.noreply.github.com>
2025-07-05 23:56:14 +0700

    config.lua

---
+++ config.lua
@@ -0,0 +1,7 @@
+return {
+    Divine    = {"Wild Galaxy Agony","Divinus","Anime Scorpion","Yin-Yang Grim Reaper","Arcane Dominus"},
+    Secret    = {"Toilet Cat","Hubert","Hot Dooooog","Hippomelon","Corn Cat","Banana"},
+    Exclusive = {"Huge Pufferfish","Huge M-6 PROTOTYPE","Huge Hell Rock","Huge Black Hole Angelus","Chest Mimic"},
+    Mythical  = {"Storm Axolotl","Rave Meebo in a Spaceship","Ghostly Dragon","Wicked Angelus","Snuggle Beast","Balloon Corgi"},
+    Legendary = {"Dominus Darkwing","Mortuus","Hacked Cat","Nightmare Cat","Inferno Cat"}
+}
